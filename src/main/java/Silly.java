bf9c97e8f3bd993b49612d75e575ac1acc2d1843
michjin <mich.jin@mail.utoronto.ca>
2022-10-09 17:24:00 -0400

    Completed CSC207 GitHub Exercise 2

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -80,6 +80,16 @@ public class Silly implements Comparable<Silly>{
      *       Make sure you document this method!
      */
 
+    /**
+     * Creates a new Silly object.
+     * This constructor takes in two Strings as an argument.
+     *
+     * @param name1 the first part of the name of this Silly instance.
+     * @param name2 the second part of the name of this Silly instance.
+     */
+    public Silly(String name1, String name2) {
+        this.name = name1 + name2;
+    }
 
 
 
@@ -116,7 +126,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -134,6 +144,8 @@ public class Silly implements Comparable<Silly>{
     @Override
     public String toString(){
         // TODO (Task 3): Implement the body of this method!
+
+        return this.name;
     }
 
     /**
@@ -157,10 +169,13 @@ public class Silly implements Comparable<Silly>{
             return false;
         }
 
+
         Silly other = (Silly) o; // To access .name of o, we need to cast it.
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+
+        return this.name.equals(other.name);
     }
 
     /**
@@ -194,6 +209,15 @@ public class Silly implements Comparable<Silly>{
          *                You can get the length of a string by using the
          *                .length() method.
          */
+
+        if (this.name.length() > other.name.length()) {
+            return 1;
+        } else if (this.name.length() < other.name.length()) {
+            return -1;
+        }
+        else {
+            return 0;
+        }
     }
 
     /*
